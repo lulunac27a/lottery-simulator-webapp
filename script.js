f9318ddaf151840d86d8d3a743c5873c636d1eec
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-01-01 17:23:00 -0600

    Add comments

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const numBallsInput = document.getElementById("num-balls");
-const totalNumbersInput = document.getElementById("total-numbers");
-const generateRandomNumberInput = document.getElementById(
+const numBallsInput = document.getElementById("num-balls"); //number of balls input
+const totalNumbersInput = document.getElementById("total-numbers"); //total numbers input
+const generateRandomNumberButton = document.getElementById(
     "generate-random-numbers",
-);
+); //generate random number button
 const outputText = document.getElementById("output");
 
-generateRandomNumberInput.addEventListener("click", () => {
-    const numBalls = numBallsInput.value;
-    const totalNumbers = totalNumbersInput.value;
-    const numbers = [];
+generateRandomNumberButton.addEventListener("click", () => {
+    //when generate random number button is pressed
+    const numBalls = numBallsInput.value; //get value of number of balls input
+    const totalNumbers = totalNumbersInput.value; //get value of total numbers input
+    const numbers = []; //numbers array
     for (let i = 0; i < totalNumbers; i++) {
-        numbers.push(i + 1);
+        //repeat for numbers in total numbers
+        numbers.push(i + 1); //add numbers to numbers array
     }
-    const selectedNumbers = [];
+    const selectedNumbers = []; //list of selected numbers
     for (let i = 0; i < numBalls; i++) {
-        const selectedNumber = Math.floor(Math.random() * numbers.length);
-        selectedNumbers.push(selectedNumber);
-        numbers.splice(selectedNumber, 1);
+        //repeat for each ball in number of balls
+        const selectedNumber = Math.floor(Math.random() * numbers.length); //get random number
+        selectedNumbers.push(selectedNumber); //add selected number to list of selected numbers
+        numbers.splice(selectedNumber, 1); //remove selected number to prevent duplicates
     }
-    outputText.innerText = numbers.join(", ");
+    outputText.innerText = numbers.join(", "); //update output text
 });
