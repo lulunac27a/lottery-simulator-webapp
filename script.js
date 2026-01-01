25eb7371733d32e754ccd335422431a67eb6de2f
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-01-01 16:52:58 -0600

    Add Prettier and format code

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const numBallsInput = document.getElementById("num-balls");
 const totalNumbersInput = document.getElementById("total-numbers");
 const generateRandomNumberInput = document.getElementById(
-  "generate-random-numbers",
+    "generate-random-numbers",
 );
 
 generateRandomNumberInput.addEventListener("click", () => {
-  const numBalls = numBallsInput.value;
-  const totalNumbers = totalNumbersInput.value;
-  const numbers = [];
-  for (let i = 0; i < totalNumbers; i++) {
-    numbers.push(i + 1);
-  }
-  const selectedNumbers = [];
-  for (let i = 0; i < numBalls; i++) {
-    const selectedNumber = Math.floor(Math.random() * numbers.length);
-    selectedNumbers.push(selectedNumber);
-    numbers.splice(selectedNumber, 1);
-  }
+    const numBalls = numBallsInput.value;
+    const totalNumbers = totalNumbersInput.value;
+    const numbers = [];
+    for (let i = 0; i < totalNumbers; i++) {
+        numbers.push(i + 1);
+    }
+    const selectedNumbers = [];
+    for (let i = 0; i < numBalls; i++) {
+        const selectedNumber = Math.floor(Math.random() * numbers.length);
+        selectedNumbers.push(selectedNumber);
+        numbers.splice(selectedNumber, 1);
+    }
 });
