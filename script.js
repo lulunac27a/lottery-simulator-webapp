0432649ea2437470a1b1c38f9a45976ae209e6fc
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-01-02 15:24:51 -0600

    Add entered numbers and matched numbers

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const numBallsInput = document.getElementById("num-balls"); //number of balls input
 const totalNumbersInput = document.getElementById("total-numbers"); //total numbers input
+const yourNumbersInput = document.getElementById("your-numbers"); //your numbers input
 const generateRandomNumberButton = document.getElementById(
     "generate-random-numbers",
 ); //generate random number button
 const outputText = document.getElementById("output"); //output text
 const generatedNumbersOutputText = document.getElementById("generated-numbers"); //generated numbers output text
+const matchedNumbersOutputText = document.getElementById("matched-numbers"); //matched numbers output text
 
 generateRandomNumberButton.addEventListener("click", () => {
     //when generate random number button is pressed
     const numBalls = parseInt(numBallsInput.value, 10); //get value of number of balls input
     const totalNumbers = parseInt(totalNumbersInput.value, 10); //get value of total numbers input
+    const yourNumbersText = yourNumbersInput.value; //get value of your numbers
+    const nums = yourNumbersText.match(/\d+/g)?.map(Number) || []; //parse string as numeric list array
+    const picks = nums.slice(0, numBalls);
     if (numBalls <= totalNumbers) {
         //if number of balls is less than or equal to total number of balls
         const numbers = []; //numbers array
@@ -24,8 +29,10 @@ generateRandomNumberButton.addEventListener("click", () => {
             selectedNumbers.push(selectedNumber); //add selected number to list of selected numbers
             numbers.splice(selectedNumber, 1); //remove selected number to prevent duplicates
         }
-        outputText.innerText = `Generated ${numBalls} balls from ${selectedNumbers} balls`; //update output text
+        const matches = selectedNumbers.filter((n) => picks.includes(n)); //find ball number matches
+        outputText.innerText = `Generated ${numBalls} balls from ${totalNumbers} balls`; //update output text
         generatedNumbersOutputText.innerText = selectedNumbers.join(", "); //update generated numbers output text
+        matchedNumbersOutputText.innerText = `${matches.length} (${matches.join(", ")}`; //update matched numbers output text
     } else {
         //if number of balls is greater than total number of balls
         alert(
