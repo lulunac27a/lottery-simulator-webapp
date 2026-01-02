f6fee08400af93e7df3da69c351cc6e828128bc4
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-01-02 11:19:47 -0600

    Add alert message if number of balls is greater than total number of balls

--- script.js
+++ script.js
@@ -10,18 +10,26 @@ generateRandomNumberButton.addEventListener("click", () => {
     //when generate random number button is pressed
     const numBalls = parseInt(numBallsInput.value, 10); //get value of number of balls input
     const totalNumbers = parseInt(totalNumbersInput.value, 10); //get value of total numbers input
-    const numbers = []; //numbers array
-    for (let i = 0; i < totalNumbers; i++) {
-        //repeat for numbers in total numbers
-        numbers.push(i + 1); //add numbers to numbers array
+    if (numBalls <= totalNumbers) {
+        //if number of balls is less than or equal to total number of balls
+        const numbers = []; //numbers array
+        for (let i = 0; i < totalNumbers; i++) {
+            //repeat for numbers in total numbers
+            numbers.push(i + 1); //add numbers to numbers array
+        }
+        const selectedNumbers = []; //list of selected numbers
+        for (let i = 0; i < numBalls; i++) {
+            //repeat for each ball in number of balls
+            const selectedNumber = Math.floor(Math.random() * numbers.length); //get random number
+            selectedNumbers.push(selectedNumber); //add selected number to list of selected numbers
+            numbers.splice(selectedNumber, 1); //remove selected number to prevent duplicates
+        }
+        outputText.innerText = `Generated ${numBalls} balls from ${selectedNumbers} balls`; //update output text
+        generatedNumbersOutputText.innerText = selectedNumbers.join(", "); //update generated numbers output text
+    } else {
+        //if number of balls is greater than total number of balls
+        alert(
+            "Number of balls must be less than or equal to total number of balls",
+        ); //alert user that number of balls must be less than or equal to total number of balls
     }
-    const selectedNumbers = []; //list of selected numbers
-    for (let i = 0; i < numBalls; i++) {
-        //repeat for each ball in number of balls
-        const selectedNumber = Math.floor(Math.random() * numbers.length); //get random number
-        selectedNumbers.push(selectedNumber); //add selected number to list of selected numbers
-        numbers.splice(selectedNumber, 1); //remove selected number to prevent duplicates
-    }
-    outputText.innerText = `Generated ${numBalls} balls from ${selectedNumbers} balls`; //update output text
-    generatedNumbersOutputText.innerText = selectedNumbers.join(", "); //update generated numbers output text
 });
